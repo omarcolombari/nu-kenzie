2ed4c86441a9fe885236e01d753d8942457eab27
Omar <omarucolombari@gmail.com>
2022-09-21 16:19:18 -0300

    Feat: instalado styled-components, criado thema e estilo global

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+const theme = {
+  colors: {
+    colorPrimary: "#FD377E",
+    colorPrimary2: "#E34981",
+    colorSecondary: "#03B898",
+    grey1: "#F8F9FA",
+    grey2: "#E9ECEF",
+    grey3: "#868E96",
+    grey4: "#212529",
+  },
+  fonts: {
+    title1: "800 38px 'Nunito'",
+    title2: "700 22px 'Nunito'",
+    title3: "700 16px 'Nunito'",
+    title4: "600 12px 'Nunito'",
+    headline: "400 16px 'Nunito'",
+    body: "400 12px 'Nunito'",
+  },
+};
+
+export default theme;
